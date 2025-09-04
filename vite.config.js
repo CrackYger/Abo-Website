5a2bc72d859f2ceddf142e7ebfacda49805b91a7
CrackYger <103510861+CrackYger@users.noreply.github.com>
2025-09-04 17:52:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig({ base: 'https://github.com/CrackYger/Abo-Website/edit/main/vite.config.js' })
+
+
 export default defineConfig({
   plugins: [react()],
   test: {
